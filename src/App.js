0b85fe35fd8ebe8d2b276258d8d0c79cc4252ecb
Josh <joshuasupp@gmail.com>
2022-09-05 22:18:07 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import React from 'react';
-import {Switch,Route} from 'react-router-dom'
-import Login from './Pages/Login/Login';
-import Home from './Pages/Home/Home';
-import NoMatch from './Pages/404Page/404Page';
-class App extends React.Component{
-  state={
-    isLog:false
-  }
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Login from "./Pages/Login/Login";
+import Home from "./Pages/Home/Home";
+import NoMatch from "./Pages/404Page/404Page";
+import Sidebar from "./Pages/Sidebar/Sidebar";
+class App extends React.Component {
+  state = {
+    isLog: false,
+  };
 
-  handleLogin = (isLog) => this.setState({isLog})
-  render(){
-    const {isLog} = this.state;
-    return(
+  handleLogin = (isLog) => this.setState({ isLog });
+  render() {
+    const { isLog } = this.state;
+    return (
       <div>
         <Switch>
-          {
-            !isLog ?
-            <Route exact path='/' render={() => <Login isLogin={this.handleLogin}/>}/>
-              :
-            <Route path='/' render={() =><Home handleLogged={this.handleLogin}/> }/>
-          }
-        <Route path='*' component={NoMatch}/>
+          {!isLog ? (
+            <Route
+              exact
+              path="/"
+              render={() => <Login isLogin={this.handleLogin} />}
+            />
+          ) : (
+            <Route
+              path="/"
+              render={() => <Home handleLogged={this.handleLogin} />}
+            />
+          )}
+          <Route path="*" component={NoMatch} />
         </Switch>
       </div>
-    )
+    );
   }
 }
 
---
+++ src/Pages/Sidebar/Sidebar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+function Sidebar() {
+  return (
+  <div> sdsds </div>
+  )
+}
+
+export default Sidebar;
