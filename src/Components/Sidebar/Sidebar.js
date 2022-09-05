fb138bddd3444d2c09f700f5f907a256422b0ba2
Josh <joshuasupp@gmail.com>
2022-09-05 23:55:49 +0000

    Update Sidebar.js

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./sidebar.css";
+import * as FaIcons from "react-icons/fa";
 
 function Sidebar() {
   return (
     <>
       <div className="Sidebar">
         <Link to="#" className="menu-bars"></Link>
+        <FaIcons.FaBars />
       </div>
     </>
   );
