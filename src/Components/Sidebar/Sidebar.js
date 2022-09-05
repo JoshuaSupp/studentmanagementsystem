a76d36678e44820feb2b9006dd34e5e2b93376be
Josh <joshuasupp@gmail.com>
2022-09-05 23:51:36 +0000

    Update 3 files

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./sidebar.css";
 
 function Sidebar() {
-  return <div className="Sidebar"> </div>;
+  return (
+    <>
+      <div className="Sidebar">
+        <Link to="#" className="menu-bars"></Link>
+      </div>
+    </>
+  );
 }
 
 export default Sidebar;
