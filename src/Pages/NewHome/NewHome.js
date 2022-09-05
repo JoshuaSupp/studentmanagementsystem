cca17ff6bcb9aaf5ee7f44a1af195d3aaab003b4
Josh <joshuasupp@gmail.com>
2022-09-05 22:25:22 +0000

    Update 4 files

--- src/Pages/NewHome/NewHome.js
+++ src/Pages/NewHome/NewHome.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import Feeds from '../../Components/Feeds/Feeds';
-import Suggestions from '../../Components/Suggestions/Suggestion';
-import './newhome.css';
-const NewHome =() =>{
-    return(
-        <div className='div-newhome'>
-            <Feeds/>
-            <Suggestions/>
-        </div>
-    )
-}
+import React from "react";
+import Feeds from "../../Components/Feeds/Feeds";
+import Suggestions from "../../Components/Suggestions/Suggestion";
+import "./newhome.css";
+const NewHome = () => {
+  return (
+    <div className="div-newhome">
+   
+    </div>
+  );
+};
 
 export default NewHome;
--- src/Pages/Sidebar/Sidebar.js
+++ src/Pages/Sidebar/Sidebar.js
@@ -2,9 +2,7 @@ import React from "react";
 import "./App.css";
 
 function Sidebar() {
-  return (
-  <div>  </div>
-  )
+  return <div> </div>;
 }
 
 export default Sidebar;
