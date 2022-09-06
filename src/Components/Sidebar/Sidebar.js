014e0689c34246dee3363f4afe00c4cdacc709ad
Josh <joshuasupp@gmail.com>
2022-09-06 00:39:01 +0000

    Update 2 files

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -3,26 +3,28 @@ import "./sidebar.css";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Sidebar() {
-    const [sidebar, setSidebar] = useState(false)
-    
-    const showSidebar = () => setSidebar(!sidebar)
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
   return (
     <>
       <div className="Sidebar">
         <Link to="#" className="menu-bars"></Link>
-        
+
         <FaIcons.FaBars />
       </div>
-      <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-          <ul className="nav-menu-items">
-            <li className="navbar-toggle">
-                <Link to="#" className="menu-bars">   
-                <AiIcons.AioutlineClose>
-                </Link>   
-         </ul>
-       </nav> 
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-items">
+          <li className="navbar-toggle">
+            <Link to="#" className="menu-bars">
+              <AiIcons.AiOutlineClose />
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
