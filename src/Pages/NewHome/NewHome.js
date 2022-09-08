961529f138938f6eb696ce3d9aa2a415d480c956
Josh <joshuasupp@gmail.com>
2022-09-08 20:19:17 +0000

    Update NewHome.js

--- src/Pages/NewHome/NewHome.js
+++ src/Pages/NewHome/NewHome.js
@@ -9,6 +9,9 @@ const NewHome = () => {
       <div class="meulabslogo">
         <Meulogo />
       </div>
+      <div class="stheading">
+        <h2> Student Management System</h2>
+      </div>
     </div>
   );
 };
