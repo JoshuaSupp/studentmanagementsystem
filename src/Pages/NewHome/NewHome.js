efcc9a36be8585041175f30bd927a9307be0d893
Josh <joshuasupp@gmail.com>
2022-09-08 21:51:27 +0000

    Update NewHome.js and newhome.css

--- src/Pages/NewHome/NewHome.js
+++ src/Pages/NewHome/NewHome.js
@@ -18,22 +18,27 @@ const NewHome = () => {
       </div>
       <h2 class="newclasses"> New Classes </h2>
       <h2>Product Design </h2>
+      <h2>Software Engineering Specialization </h2>
       <div class="row">
         <div class="column">
           <img
+            className="imgclass"
             src="https://meulabs.org/wp-content/uploads/2022/08/why-enjoy-img-1-1.webp"
             alt=""
           />
         </div>
-        <h2>Software Engineering Specialization </h2>
+
         <div class="column">
           <img
+            className="imgclass"
             src="https://us.123rf.com/450wm/garagestock/garagestock1605/garagestock160507748/57581527-technology-concept-software-engineering.jpg?ver=6"
             alt=""
           />
         </div>
+        <h2>Creative Expressions </h2>
         <div class="column">
           <img
+            className="imgclass"
             src="https://cdn.mos.cms.futurecdn.net/FQiMdRcDKGsgcxsMfhoXm3.jpg"
             alt=""
           />
